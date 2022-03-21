d9265e1065b1819ef3a5dabcfa6a58b37ecbc151
Camilla <camilla.lima000@gmail.com>
2022-03-21 00:25:17 +0000

    Updated the form to use formik

--- src/components/Costumer/NewCostumerForm.js
+++ src/components/Costumer/NewCostumerForm.js
@@ -1,22 +1,38 @@
 import React from "react";
 import "./NewCostumerForm.css";
+import { useFormik } from "formik";
 import StudentLifeCard from "../Cards/StudentLifeCard";
 import AnywhereCard from "../Cards/AnywhereCard";
 import LiquidCard from "../Cards/LiquidCard";
 
 function NewCostumerForm() {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      firstName: "",
+      middleName: "",
+      lastName: "",
+      dob: "",
+      jobStatus: "",
+      income: "",
+    },
+  });
+
+  console.log(formik.values);
 
   return (
     <>
       <div className="NewCostumerForm">
         <h1>Check your eligibility </h1>
+
         <form action="" className="formData">
           <label>Title </label>
           <select
-            name="title"
-            id=""
             className="selectInput"
-            defaultValue="Choose..."
+            id="title"
+            name="title"
+            onChange={formik.handleChange}
+            value={formik.values.title}
           >
             <option value="Mr.">Mr.</option>
             <option value="Mrs.">Mrs.</option>
@@ -28,38 +44,59 @@ function NewCostumerForm() {
           <label>First Name</label>
           <br />
           <input
+            id="firstName"
             type="text"
             name="firstName"
             placeholder="First name"
             className="formInput"
-            
+            onChange={formik.handleChange}
+            value={formik.values.firstName}
           />
           <br />
           <label>Middle Name</label>
           <br />
           <input
+            id="middleName"
             type="text"
             name="middleName"
             className="formInput"
             placeholder="Middle Name"
+            onChange={formik.handleChange}
+            value={formik.values.middleName}
           />
           <br />
           <label>Last Name</label>
           <br />
           <input
+            id="lastName"
             type="text"
             name="lastName"
             className="formInput"
             placeholder="Last Name"
+            onChange={formik.handleChange}
+            value={formik.values.lastName}
           />
           <br />
           <label>Date of Birth</label>
           <br />
-          <input type="date" name="dob" className="formInput" />
+          <input
+            id="dob"
+            type="date"
+            name="dob"
+            className="formInput"
+            onChange={formik.handleChange}
+            value={formik.values.dob}
+          />
           <br />
           <label>Job Status</label>
           <br />
-          <select name="jobStatus" className="formInput">
+          <select
+            id="jobStatus"
+            name="jobStatus"
+            className="formInput"
+            onChange={formik.handleChange}
+            value={formik.values.jobStatus}
+          >
             <option value="Employed">Employed Full Time</option>
             <option value="Employed">Employed Part Time</option>
             <option value="Self-Employed">Self-Employed</option>
@@ -71,24 +108,26 @@ function NewCostumerForm() {
           <br />
           <input
             type="text"
-            name="annualIncome"
+            id="income"
+            name="income"
             placeholder="£"
             className="formInput"
+            onChange={formik.handleChange}
+            value={formik.values.income}
           />
           <br />
           <br />
-          
-  
-          <button type="submit" className="submitButton" >
+
+          <button type="submit" className="submitButton">
             Submit
           </button>
 
           <br />
         </form>
       </div>
-      < StudentLifeCard />
-      < AnywhereCard />
-      < LiquidCard />
+      <StudentLifeCard />
+      <AnywhereCard />
+      <LiquidCard />
       <br />
       <br />
     </>
