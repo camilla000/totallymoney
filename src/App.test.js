b5cf2f999038f08eb757aa9a18570232b50a7e90
Camilla <camilla.lima000@gmail.com>
2022-03-22 20:51:52 +0000

    Added first test to check if page display text

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Check eligibility", () => {
+  it("should render the title", () => {
+    render(<App />);
+    expect(screen.getByText("Check your eligibility")).toBeInTheDocument();
+  });
 });
